917bc7264198d1efa62b18ece9e0235b03b0c7ee
NadaSOsman <76102938+NadaSOsman@users.noreply.github.com>
2021-06-22 11:40:29 +0200

    Update download_pretrained_models.sh

--- scripts/download_pretrained_models.sh
+++ scripts/download_pretrained_models.sh
@@ -13,10 +13,14 @@ cd models/ek55_0.125
 curl https://transfer.sh/1tYUQD7/RULSTM-anticipation_0.125_24_16_flow_best.pth.tar -o RULSTM-anticipation_0.125_24_16_flow_best.pth.tar
 curl https://transfer.sh/12Q1oqp/RULSTM-anticipation_0.125_24_16_obj_best.pth.tar -o RULSTM-anticipation_0.125_24_16_obj_best.pth.tar
 curl https://transfer.sh/1uEWmXy/RULSTM-anticipation_0.125_24_16_rgb_best.pth.tar -o RULSTM-anticipation_0.125_24_16_rgb_best.pth.tar
-----------------curl https://transfer.sh/1VvVhxj/RULSTM-anticipation_0.5_6_4_rgb_best.pth.tar -o RULSTM-anticipation_0.5_6_4_rgb_best.pth.tar
+curl https://transfer.sh/164VkkV/RULSTM-anticipation_0.125_12_16_fusion_best.pth.tar -o RULSTM-anticipation_0.125_12_16_fusion_best.pth.tar
 
 cd ../../
 mkdir -p models/ek55
 cd models/ek55
 
-
+curl https://transfer.sh/1nFKMAZ/RULSTM-anticipation_0.125_24_16_0.5_6_4_flow_best.pth.tar -o RULSTM-anticipation_0.125_24_16_0.5_6_4_flow_best.pth.tar
+curl https://transfer.sh/1dIDg8c/RULSTM-anticipation_0.125_24_16_0.5_6_4_obj_best.pth.tar -o RULSTM-anticipation_0.125_24_16_0.5_6_4_obj_best.pth.ta
+curl https://transfer.sh/1tbr9bV/RULSTM-anticipation_0.125_24_16_0.5_6_4_rgb_best.pth.tar -o RULSTM-anticipation_0.125_24_16_0.5_6_4_rgb_best.pth.tar
+curl https://transfer.sh/1AYBqh5/RULSTM-anticipation_0.125_24_16_0.5_6_4_fusion.pth.tar -o RULSTM-anticipation_0.125_24_16_0.5_6_4_fusion.pth.tar
+curl https://transfer.sh/1EvqkpX/RULSTM-anticipation_0.125_24_16_0.5_6_4_fusion_best.pth.tar -o RULSTM-anticipation_0.125_24_16_0.5_6_4_fusion_best.pth.tar
