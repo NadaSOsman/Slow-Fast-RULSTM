4b78b5e46e34fefaaf082d880ff0f881730ddc30
NadaSOsman <76102938+NadaSOsman@users.noreply.github.com>
2021-06-23 16:58:09 +0200

    Add files via upload

---
+++ scripts/download_models_ek55.sh
@@ -0,0 +1,9 @@
+mkdir -p models/ek55
+cd models/ek55
+curl https://iplab.dmi.unict.it/sharing/rulstm/iccv_models/RULSTM-anticipation_0.25_6_8_rgb_best.pth.tar -O
+curl https://iplab.dmi.unict.it/sharing/rulstm/iccv_models/RULSTM-anticipation_0.25_6_8_flow_best.pth.tar -O
+curl https://iplab.dmi.unict.it/sharing/rulstm/iccv_models/RULSTM-anticipation_0.25_6_8_obj_best.pth.tar -O
+curl https://iplab.dmi.unict.it/sharing/rulstm/iccv_models/RULSTM-anticipation_0.25_6_8_fusion_best.pth.tar -O
+curl https://iplab.dmi.unict.it/sharing/rulstm/iccv_models/RULSTM-early_recognition_0.25_8_rgb_best.pth.tar -O
+curl https://iplab.dmi.unict.it/sharing/rulstm/iccv_models/RULSTM-early_recognition_0.25_8_flow_best.pth.tar -O
+curl https://iplab.dmi.unict.it/sharing/rulstm/iccv_models/RULSTM-early_recognition_0.25_8_obj_best.pth.tar -O
